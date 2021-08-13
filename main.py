d9b4a36a0e8cd72f46743aa0d3d1ae00ad8cd52a
box16 <inoinobox16@gmail.com>
2021-08-13 20:47:37 +0900

    ランダム移動実装

--- main.py
+++ main.py
@@ -14,12 +14,20 @@ class Field():
     def print_field(self):
         for line in self.field:
             for element in line:
-                print(element,end="\t")
+                print(element,end="  ")
             print()
         print("\n=======================\n")
     
     def update_field(self,row,column,mark="-"):
         self.field[row][column] = mark
+    
+    def check_field_over(self,row,column):
+        if (row < 0) or (row >= len(self.field)):
+            return True
+        elif (column < 0) or (column >= len(self.field[0])):
+            return True
+        else:
+            return False
 
 class Predator():
     def __init__(self):
@@ -37,22 +45,34 @@ class Predator():
     def get_pos(self):
         return (self.pos_row,self.pos_column)
 
+def suggest_next_pos(row,column):
+    move_direction = [-1,0,1]
+    move_row = random.choice(move_direction)
+    move_column = random.choice(move_direction)
+    next_row = row + move_row
+    next_column = column + move_column
+    return (next_row,next_column)
+
 if __name__=="__main__":
-    ROW = 5
-    COLUMN = 5
+    ROW = 10
+    COLUMN = 10
     field = Field(ROW,COLUMN)
     predator = Predator()
 
     field.print_field()
 
-    for i in range(10):
+    while(True):
         # Predatorの現在位置取得
         now_pos = predator.get_pos()
 
         # Predatorの移動位置決定
-        next_row = random.choice(range(ROW))
-        next_column = random.choice(range(COLUMN))
-        predator.update_pos(next_row,next_column)
+        next_pos = suggest_next_pos(now_pos[0],now_pos[1])
+        if field.check_field_over(next_pos[0],next_pos[1]):
+            continue
+        elif now_pos == next_pos:
+            continue
+        else:
+            predator.update_pos(next_pos[0],next_pos[1])
 
         # Fieldの更新
         field.update_field(now_pos[0],now_pos[1])
